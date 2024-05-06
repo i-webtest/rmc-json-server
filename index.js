f07b8cf022dd3ceeaa9ccef6137623174a28301c
i-webtest <stasbass80@gmail.com>
2024-05-06 19:13:37 +0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // const jsonServer = require('json-server');
+const jsonServer = require('json-server');
 
-import jsonServer from 'json-server';
+// import jsonServer from 'json-server';
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
